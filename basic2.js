aa1e69f9893a15993fa70eb900d157967a0887f7
BarnoMamadzhanova <b@mamadzhanova.com>
2023-10-04 13:56:24 +0200

    creating class animals

--- basic2.js
+++ basic2.js
@@ -1,2 +1,31 @@
 
 
+let animalsArr = [];
+class animals {
+    constructor(species, name, breed, age, food) {
+        this.species = species;
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.food = food;
+        animalsArr.push(this)
+    }
+
+    printInfo() {
+        return `My ${this.species} ${this.name} likes ${this.food}.`
+    }
+}
+
+const animal1 = new animals("dog", "Coco", "doberman", 2, ["fish", "meat"]);
+const animal2 = new animals("cat", "Sisi", "safary", 1, ["milk", "dry food"]);
+const animal3 = new animals("fish", "Yellow", "guppy", 1, ["worm", "caviar"]);
+console.log(animalsArr);
+
+for(let animals of animalsArr) {
+    document.getElementById('print').innerHTML +=
+    `<ul class="list-group my-2 p-2">
+        <li class="list-group-item">${animals.printInfo()}</li>
+    </ul>
+    `
+}
+
